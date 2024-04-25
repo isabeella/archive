b23b3312b87e63830ffac40a513b9131616c131b
Isabella <iv24223@westridge.org>
2024-04-24 21:20:13 -0700

    admin email settings working!!!

--- models/Preference.js
+++ models/Preference.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const validator = require('validator'); 
 
-// pref1 - emails to reviewers when new article is submitted
+//pref2 - emails to contributors when their articles are reviewed and need edits
 //
-// pref2 - emails to contributors when their articles are reviewed
+//pref3 - emails to assigned reviewers when the contributor submits their edits
 //
-// pref3 - emails to assigned reviewers when the contributor submits their edits
+//pref4 - emails to contributor when their article gets published
 //
-// pref4 - emails to contributor when their article gets published
+//pref5 - emails to level 2 reviewers when new article is submitted
+//
+//pref6 - emails to level 3 reviewers when new article is submitted
+//
+//pref7 - emails to level 4 reviewers when new article is submitted
 
 const preferenceSchema = new mongoose.Schema({
-  pref1: {
-    type: Boolean,
-    default: true
-  },
   pref2: {
     type: Boolean,
     default: true
@@ -24,6 +24,18 @@ const preferenceSchema = new mongoose.Schema({
     default: true
   },
   pref4: {
+    type: Boolean,
+    default: true
+  },
+  pref5: {
+    type: Boolean,
+    default: true
+  },
+  pref6: {
+    type: Boolean,
+    default: true
+  },
+  pref7: {
     type: Boolean,
     default: true
   }
