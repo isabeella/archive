28191c993bc11fd86cba49368a9f669352220784
Isabella <iv24223@westridge.org>
2024-05-22 10:17:52 -0700

    new settings and emails

--- models/Preference.js
+++ models/Preference.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const validator = require('validator'); 
 
+//pref1 - emails to 3s and 4s when someone goes from 0 to 1
+//
 //pref2 - emails to contributors when their articles are reviewed and need edits
 //
 //pref3 - emails to assigned reviewers when the contributor submits their edits
@@ -13,8 +15,14 @@ const validator = require('validator');
 //pref6 - emails to level 3 reviewers when new article is submitted
 //
 //pref7 - emails to level 4 reviewers when new article is submitted
+//
+//pref8 - approval email when you become a reviewer
 
 const preferenceSchema = new mongoose.Schema({
+  pref1: {
+    type: Boolean,
+    default: true
+  },
   pref2: {
     type: Boolean,
     default: true
@@ -36,6 +44,10 @@ const preferenceSchema = new mongoose.Schema({
     default: true
   },
   pref7: {
+    type: Boolean,
+    default: true
+  },
+  pref8: {
     type: Boolean,
     default: true
   }
